2f7c05a59d12439ad60540eb1266b262eec8ef5b
Aswanth-10 <aswanth.n.dev@gmail.com>
2026-04-01 22:35:16 +0530

    feat: use custom team names

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,16 +3,16 @@ const router = express.Router();
 
 const users = [
     { id: 'admin', role: 'admin', password: 'admin123' },
-    { id: 'teamA', role: 'team', password: 'password', name: 'Team A', currency: 1000, points: 0 },
-    { id: 'teamB', role: 'team', password: 'password', name: 'Team B', currency: 1000, points: 0 },
-    { id: 'teamC', role: 'team', password: 'password', name: 'Team C', currency: 1000, points: 0 },
-    { id: 'teamD', role: 'team', password: 'password', name: 'Team D', currency: 1000, points: 0 },
-    { id: 'teamE', role: 'team', password: 'password', name: 'Team E', currency: 1000, points: 0 },
-    { id: 'teamF', role: 'team', password: 'password', name: 'Team F', currency: 1000, points: 0 },
-    { id: 'teamG', role: 'team', password: 'password', name: 'Team G', currency: 1000, points: 0 },
-    { id: 'teamH', role: 'team', password: 'password', name: 'Team H', currency: 1000, points: 0 },
-    { id: 'teamI', role: 'team', password: 'password', name: 'Team I', currency: 1000, points: 0 },
-    { id: 'teamJ', role: 'team', password: 'password', name: 'Team J', currency: 1000, points: 0 },
+    { id: 'teamA', role: 'team', password: 'password', name: 'Thinkers Duo', currency: 1000, points: 0 },
+    { id: 'teamB', role: 'team', password: 'password', name: 'Hack & Code', currency: 1000, points: 0 },
+    { id: 'teamC', role: 'team', password: 'password', name: 'Newbies', currency: 1000, points: 0 },
+    { id: 'teamD', role: 'team', password: 'password', name: 'Boult', currency: 1000, points: 0 },
+    { id: 'teamE', role: 'team', password: 'password', name: 'Error 404', currency: 1000, points: 0 },
+    { id: 'teamF', role: 'team', password: 'password', name: 'Solitude', currency: 1000, points: 0 },
+    { id: 'teamG', role: 'team', password: 'password', name: 'Code Bidders', currency: 1000, points: 0 },
+    { id: 'teamH', role: 'team', password: 'password', name: 'Debuggers', currency: 1000, points: 0 },
+    { id: 'teamI', role: 'team', password: 'password', name: 'Hackos', currency: 1000, points: 0 },
+    { id: 'teamJ', role: 'team', password: 'password', name: 'Decoders', currency: 1000, points: 0 },
 ];
 
 router.post('/login', (req, res) => {
